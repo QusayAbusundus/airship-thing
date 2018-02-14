4d2709bc1c98b69295990824cc8d06ade66e9608
QusayAbusundus <32073337+QusayAbusundus@users.noreply.github.com>
2018-02-14 13:40:01 -0500

    Added movement

--- Airship Game/Assets/Scripts/PlayerController.cs
+++ Airship Game/Assets/Scripts/PlayerController.cs
@@ -2,13 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour 
 {
+	public int speed;
+	public float tilt;
+	private Rigidbody rigidbody;
+	
+	void Start()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+	}
+	
 	void FixedUpdate()
 	{
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
 		
-		rigidbody.velocity
+		Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+		rigidbody.velocity = movement * speed;
+		
+		rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidbody.velocity.x * -tilt);
 	}
 }
