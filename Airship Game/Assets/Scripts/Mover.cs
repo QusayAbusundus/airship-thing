d56c9478738dc8d078514d36f5f325ee27f1e302
QusayAbusundus <32073337+QusayAbusundus@users.noreply.github.com>
2018-02-15 13:40:18 -0500

    Added bolt

---
+++ Airship Game/Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour 
+{
+	public float speed;
+	private Rigidbody RB;
+	
+	void Start()
+	{
+		RB = GetComponent<Rigidbody>();
+		
+		RB.velocity = transform.forward * speed;
+	}
+}
