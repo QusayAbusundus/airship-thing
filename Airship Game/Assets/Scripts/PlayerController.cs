0155dd9b2d7ea292cd69d1b138af61462f5487b0
QusayAbusundus <32073337+QusayAbusundus@users.noreply.github.com>
2018-02-21 13:38:55 -0500

    Finished shooting

--- Airship Game/Assets/Scripts/PlayerController.cs
+++ Airship Game/Assets/Scripts/PlayerController.cs
@@ -2,23 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, yMin, yMax, zMin, zMax;
+}
+
 public class PlayerController : MonoBehaviour 
 {
 	public int speed;
-	public float tilt;
+	public float tilt;	
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	
+	public Boundary boundary;
+	
 	private Rigidbody rigidbody;
-	
-	
+	private float nextFire;
 	
 	void Start()
 	{
 		rigidbody = GetComponent<Rigidbody>();
 	}
 	
-	/*void Update()
+	void Update()
 	{
-		Instantiate(object, position, rotation);
-	}*/
+		if(Input.GetButton("Fire1") & Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
 	
 	void FixedUpdate()
 	{
@@ -27,6 +42,13 @@ public class PlayerController : MonoBehaviour
 		
 		Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
 		rigidbody.velocity = movement * speed;
+		
+		rigidbody.position = new Vector3
+		(
+			Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+			Mathf.Clamp(rigidbody.position.y, boundary.yMin, boundary.yMax),
+			0.0f
+		);
 		
 		rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidbody.velocity.x * -tilt);
 	}
