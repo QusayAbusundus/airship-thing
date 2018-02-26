455e0cc7002393b982b3114b3cb3ea6a035fbdb7
QusayAbusundus <32073337+QusayAbusundus@users.noreply.github.com>
2018-02-26 13:41:03 -0500

    Finished aestroids

---
+++ Airship Game/Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour 
+{
+	public GameObject hazard;
+	public Transform boundary;
+	
+	void Start()
+	{
+		spawnWaves();
+	}
+	
+	void spawnWaves()
+	{
+		Vector3 spawnPosition = new Vector3
+		(
+			Random.Range(-(boundary.localScale.x/2), (boundary.localScale.x/2)),
+			Random.Range(-(boundary.localScale.y/2), (boundary.localScale.y/2)), 
+			boundary.localScale.z
+		);
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate(hazard, spawnPosition, spawnRotation);
+	}
+}
