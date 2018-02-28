5e1d3e45e0373fd994b307a96388bd26bfe5ce40
QusayAbusundus <32073337+QusayAbusundus@users.noreply.github.com>
2018-02-28 13:40:29 -0500

    wave 1

--- Airship Game/Assets/Scripts/GameController.cs
+++ Airship Game/Assets/Scripts/GameController.cs
@@ -6,21 +6,29 @@ public class GameController : MonoBehaviour
 {
 	public GameObject hazard;
 	public Transform boundary;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
 	
 	void Start()
 	{
-		spawnWaves();
+		StartCoroutine(spawnWaves());
 	}
 	
-	void spawnWaves()
+	IEnumerator spawnWaves()
 	{
-		Vector3 spawnPosition = new Vector3
-		(
-			Random.Range(-(boundary.localScale.x/2), (boundary.localScale.x/2)),
-			Random.Range(-(boundary.localScale.y/2), (boundary.localScale.y/2)), 
-			boundary.localScale.z
-		);
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate(hazard, spawnPosition, spawnRotation);
+		yield return new WaitForSeconds(startWait);
+		for(int i = 0; i < hazardCount; i++)
+		{
+			Vector3 spawnPosition = new Vector3
+			(
+				Random.Range(-(boundary.localScale.x/2), (boundary.localScale.x/2)),
+				Random.Range(-(boundary.localScale.y/2), (boundary.localScale.y/2)), 
+				boundary.localScale.z
+			);
+			Quaternion spawnRotation = Quaternion.identity;
+			Instantiate(hazard, spawnPosition, spawnRotation);
+			yield return new WaitForSeconds(spawnWait);
+		}
 	}
 }
