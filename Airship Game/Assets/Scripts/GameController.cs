b867c9adb1d836eadcf412caa06c0610f5a43f2e
QusayAbusundus <32073337+QusayAbusundus@users.noreply.github.com>
2018-03-01 13:41:00 -0500

    Fixed asteroids

--- Airship Game/Assets/Scripts/GameController.cs
+++ Airship Game/Assets/Scripts/GameController.cs
@@ -7,8 +7,9 @@ public class GameController : MonoBehaviour
 	public GameObject hazard;
 	public Transform boundary;
 	public int hazardCount;
-	public float spawnWait;
 	public float startWait;
+	public float spawnWait;
+	public float waveWait;
 	
 	void Start()
 	{
@@ -18,17 +19,21 @@ public class GameController : MonoBehaviour
 	IEnumerator spawnWaves()
 	{
 		yield return new WaitForSeconds(startWait);
-		for(int i = 0; i < hazardCount; i++)
+		while(true)
 		{
-			Vector3 spawnPosition = new Vector3
-			(
-				Random.Range(-(boundary.localScale.x/2), (boundary.localScale.x/2)),
-				Random.Range(-(boundary.localScale.y/2), (boundary.localScale.y/2)), 
-				boundary.localScale.z
-			);
-			Quaternion spawnRotation = Quaternion.identity;
-			Instantiate(hazard, spawnPosition, spawnRotation);
-			yield return new WaitForSeconds(spawnWait);
+			for(int i = 0; i < hazardCount; i++)
+			{
+				Vector3 spawnPosition = new Vector3
+				(
+					boundary.position.x	+ Random.Range(-(boundary.localScale.x/2), (boundary.localScale.x/2)),
+					boundary.position.y + Random.Range(-(boundary.localScale.y/2), (boundary.localScale.y/2)), 
+					boundary.localScale.z/2
+				);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate(hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds(spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
 		}
 	}
 }
